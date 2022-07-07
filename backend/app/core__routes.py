a520a63b455be760bc49b15a4403f173df2b759b
Pierpaolo Calanna <p.calanna@gmail.com>
2022-07-07 18:37:25 +0200

    added tags to routers

--- backend/app/core__routes.py
+++ backend/app/core__routes.py
@@ -5,5 +5,9 @@ import routes__users
 import routes__scrape
 
 def add_routes(app: FastAPI):
-  for routes in [routes__lemmi, routes__users, routes__scrape]:
-    app.include_router(routes.router)
+  for routes, tags in [
+    (routes__users, ["users"]),
+    (routes__lemmi, ["lemmi"]), 
+    (routes__scrape, ["scrape"]),
+  ]:
+    app.include_router(routes.router, tags=tags)
