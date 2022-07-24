a1c0a3e2486cec6808d452faec21008dba1a63e1
Pierpaolo Calanna <p.calanna@gmail.com>
2022-07-24 09:28:43 +0200

    extracting forms from views

---
+++ frontend/app/src/views/Forms__Login.vue
@@ -0,0 +1,103 @@
+<template>
+  <Form
+    v-slot="{ errors, isSubmitting, values: { username, password } }"
+    :validation-schema="validationSchema"
+    :initial-values="{
+      username: 'p.calanna@gmail.com',
+      password: '',
+    }"
+    @submit="onSubmitForm"
+  >
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control">
+        <Field
+          name="username"
+          type="text"
+          class="input is-medium"
+          placeholder="mario.rossi@mail.com"
+          :class="{ 'is-danger': errors.username }"
+        />
+        <div class="has-text-danger">
+          {{ errors.username }}
+        </div>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Password</label>
+      <div class="control">
+        <Field
+          name="password"
+          type="password"
+          class="input is-medium"
+          placeholder="password di mario rossi"
+          :class="{ 'is-danger': errors.password }"
+        />
+        <div class="has-text-danger">
+          {{ errors.password }}
+        </div>
+      </div>
+    </div>
+    <div class="control mt-5">
+      <base-loading-button
+        v-model="isLoading"
+        :type="'submit'"
+        :button-css="'is-medium is-info'"
+        :disabled="isSubmitting || !(username && password)"
+      >
+        Effettua login
+      </base-loading-button>
+    </div>
+    <div v-if="errors.apiError" class="has-text-danger mt-3 mb-0">
+      {{ errors.apiError }}
+    </div>
+  </Form>
+</template>
+
+<script setup>
+/* eslint-disable no-unused-vars */
+import { ref, watch, onMounted } from "vue";
+import { configure, Form, Field } from "vee-validate";
+import { object, string } from "yup";
+
+const props = defineProps({
+  resultIsReady: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits({
+  "on-login": null,
+});
+
+configure({
+  validateOnBlur: false,
+  validateOnChange: true,
+  validateOnInput: false,
+  validateOnModelUpdate: true,
+});
+
+const validationSchema = object().shape({
+  username: string().email("email non valida").required("email richiesta"),
+  password: string().required("password richiesta"),
+});
+
+const isLoading = ref(false);
+
+watch(
+  () => props.resultIsReady,
+  (value) => {
+    if (value) isLoading.value = false;
+  }
+);
+
+const onSubmitForm = async ({ username, password }, { setErrors }) => {
+  emit("on-login", { formData: { username, password }, setErrors });
+};
+
+onMounted(() => {
+  const lemmaInput = document.getElementsByTagName("input")[0];
+  lemmaInput.focus();
+});
+</script>
--- frontend/app/src/views/Views__Login.vue
+++ frontend/app/src/views/Views__Login.vue
@@ -1,98 +1,32 @@
 <template>
   <div>
     <base-title>Login</base-title>
-    <Form
-      v-slot="{ errors, isSubmitting, values: { username, password } }"
-      :validation-schema="validationSchema"
-      :initial-values="{
-        username: 'p.calanna@gmail.com',
-        password: '',
-      }"
-      @submit="onSubmitForm"
-    >
-      <div class="field">
-        <label class="label">Email</label>
-        <div class="control">
-          <Field
-            name="username"
-            type="text"
-            class="input is-medium"
-            placeholder="mario.rossi@mail.com"
-            :class="{ 'is-danger': errors.username }"
-          />
-          <div class="has-text-danger">
-            {{ errors.username }}
-          </div>
-        </div>
-      </div>
-      <div class="field">
-        <label class="label">Password</label>
-        <div class="control">
-          <Field
-            name="password"
-            type="password"
-            class="input is-medium"
-            placeholder="password di mario rossi"
-            :class="{ 'is-danger': errors.password }"
-          />
-          <div class="has-text-danger">
-            {{ errors.password }}
-          </div>
-        </div>
-      </div>
-      <div class="control mt-5">
-        <base-loading-button
-          v-model="isLoading"
-          :type="'submit'"
-          :button-css="'is-medium is-info'"
-          :disabled="isSubmitting || !(username && password)"
-        >
-          Effettua login
-        </base-loading-button>
-      </div>
-      <div v-if="errors.apiError" class="has-text-danger mt-3 mb-0">
-        {{ errors.apiError }}
-      </div>
-    </Form>
+    <login-form 
+      :result-is-ready="resultIsReady"
+      @on-login="onLogin"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import { configure, Form, Field } from "vee-validate";
-import { object, string } from "yup";
+import { ref } from "vue";
 
 import { useAuthStore } from "../store__auth";
 import { router } from "../router__main";
+import LoginForm from "./Forms__Login.vue";
 
-configure({
-  validateOnBlur: false,
-  validateOnChange: true,
-  validateOnInput: false,
-  validateOnModelUpdate: true,
-});
+const resultIsReady = ref(false);
 
-const validationSchema = object().shape({
-  username: string().email("email non valida").required("email richiesta"),
-  password: string().required("password richiesta"),
-});
-
-const isLoading = ref(false);
-
-const onSubmitForm = async ({ username, password }, { setErrors }) => {
+const onLogin = async ({ formData: { username, password }, setErrors }) => {
   try {
+    resultIsReady.value = false;
     const { login } = useAuthStore();
     const successfulLogin = await login(username, password);
     router.push({ name: successfulLogin ? "route-list" : "route-login" });
   } catch (error) {
     setErrors({ apiError: error });
   } finally {
-    isLoading.value = false;
+    resultIsReady.value = true;
   }
 };
-
-onMounted(() => {
-  const lemmaInput = document.getElementsByTagName("input")[0];
-  lemmaInput.focus();
-});
 </script>
